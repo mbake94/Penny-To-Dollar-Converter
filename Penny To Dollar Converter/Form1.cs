6a09e692ce8522199017b23dbe9111aba7ea0704
mbake94 <mbake94@gmail.com>
2018-01-17 23:51:21 -0700

    Add files via upload

---
+++ Penny To Dollar Converter/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Penny_To_Dollar_Converter
+{ 
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnConvert_Click(object sender, EventArgs e)
+		{
+			int dollars, quarters, dimes, nickles, pennies;
+			int totalPennies;
+
+			totalPennies = int.Parse(txtInput.Text);
+			quarters = int.Parse(txtQuarters.Text);
+			dollars = int.Parse(txtDollars.Text);
+			dimes = int.Parse(txtDimes.Text);
+			nickles = int.Parse(txtNickles.Text);
+			pennies = int.Parse(txtPennies.Text);
+
+
+			dollars = totalPennies % 100;
+			quarters = totalPennies % 25;
+			dimes = totalPennies % 10;
+			nickles = totalPennies % 5;
+			pennies = totalPennies;
+
+
+			txtDollars.Text = dollars.ToString();
+			txtQuarters.Text = quarters.ToString();
+			txtDimes.Text = dimes.ToString();
+			txtNickles.Text = nickles.ToString();
+			txtPennies.Text = pennies.ToString();
+		}
+	}
+	
+}
